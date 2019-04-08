323f4dd93bb39aff7453b56313e3ab7e336f8d28
alumno <alumno>
2019-04-08 12:12:54 -0300

    Cadenas

--- Cadenas/main.c
+++ Cadenas/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char palabra[10]; //Siempre necesito uno mas para la finalizacion "\0"
+    int len;
+    char palabra[10]; //Siempre necesito uno mas para la finalizacion '\0'
     printf("Ingrese nombre y apellido: ");
-    //scanf("%s", palabra); Se frena cuando encuentra un espacio
-    //scanf("[^\n]", palabra); Funciona en W y L
-    //fgets(palabra, 10, stdin); Funciona en W y L
     gets(palabra); //Solo funciona en Windows
-    printf("\n%s\n", palabra);
+    len = strlen(palabra); //Permite conocer el tamaño en bytes de una cadena
+    printf("\nLa palabra es: %s\nCantidad de caracteres: %d", palabra, len);
     return 0;
 }
+
+//scanf("%s", palabra); Se frena cuando encuentra un espacio
+
+//scanf("[^\n]", palabra); Funciona en W y L
+
+//fgets(palabra, 10, stdin); Funciona en W y L, pensada para leer lineas de un archivo. Al usar strlen, lee tambien el \n
+//len = strlen(palabra);
+//palabra[len] = '\0'
