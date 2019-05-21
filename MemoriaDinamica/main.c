8a1acfc0d44dffd87454c78ed53cab06615566b8
Alan Passucci <47321306+WikyPassu@users.noreply.github.com>
2019-05-21 12:34:23 -0300

    Add files via upload

--- MemoriaDinamica/main.c
+++ MemoriaDinamica/main.c
@@ -1,9 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define T 5
+
+int cargarVector(int* pArray, int sizeA);
+int mostrarVector(int* pArray, int sizeA);
 
 int main()
 {
-    /*
+    //Vectores.
+    int* pNumeros;
+    int* aux;
+    int i, t = T+5;
+
+    pNumeros = (int*) malloc(sizeof(int)*5); //Pido 20 bytes de memoria consecutiva en el Heap.
+
+    for(i=0; i<T; i++)
+        {
+            *(pNumeros+i) = i+1;
+        }
+
+    aux = (int*) realloc(pNumeros, sizeof(int)*t);
+
+    if(aux != NULL)
+    {
+        pNumeros = aux;
+        cargarVector(pNumeros, t);
+        mostrarVector(pNumeros, t);
+    }
+
+    printf("\n");
+
+
+    //pNumeros = realloc(pNumeros, sizeof(int)*(T+5));
+    /*if(pNumeros != NULL)
+    {
+        for(i=0; i<T; i++)
+        {
+            printf("%d. Ingrese un numero: ", i+1);
+            scanf("%d", pNumeros+i);
+        }
+        printf("\n");
+
+        for(i=0; i<T; i++)
+        {
+            printf("%d ", *(pNumeros+i));
+        }
+        printf("\n");
+    }
+    */
+    return 0;
+}
+
+int cargarVector(int* pArray, int sizeA)
+{
+    int i, estado = 0;
+    if(pArray != NULL)
+    {
+        for(i=0; i<sizeA; i++)
+        {
+            printf("%d. Ingrese un numero: ", i+1);
+            scanf("%d", pArray+i);
+        }
+        estado = 1;
+    }
+    return estado;
+}
+
+int mostrarVector(int* pArray, int sizeA)
+{
+    int i, estado = 0;
+    if(pArray != NULL)
+    {
+        for(i=0; i<sizeA; i++)
+        {
+            printf("%d ", *(pArray+i));
+        }
+        estado = 1;
+    }
+    return estado;
+}
+
+/*
     int* pNumero; //Esta en la pila.
 
     //Malloc devuelve void, lo casteo a int para poder guardarlo en pNumero y le paso el sizeof de un int.
@@ -24,28 +101,3 @@ int main()
         printf("Valor -> *pNumero: %d\n", *pNumero); //Muestra valor.
     }
     */
-
-    //Vectores.
-
-    int* pNumeros;
-    int i;
-
-    pNumeros = (int*) malloc(sizeof(int)*5); //Pido 20 bytes de memoria consecutiva en el Heap.
-
-    if(pNumeros != NULL)
-    {
-        for(i=0; i<5; i++)
-        {
-            printf("%d. Ingrese un numero: ", i+1);
-            scanf("%d", pNumeros+i);
-        }
-        printf("\n");
-        for(i=0; i<5; i++)
-        {
-            printf("%d ", *(pNumeros+i));
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
